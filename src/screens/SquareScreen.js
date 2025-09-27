c2da96b8ae72481e2003d17c61fde2d056731b8a
Merdan <wisew.tube@gmail.com>
2025-09-28 02:09:20 +0500

    init commit

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,30 +1,48 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import ColorCounter from "../components/ColorCounter";
+
+const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
-  const [blue, setBlue] = useState(0);
   const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
 
-  console.log(red);
+  const setColor = (color, change) => {
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+    }
+  };
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setColor("red", COLOR_INCREMENT)}
+        onDecrease={() => setColor("red", -COLOR_INCREMENT)}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setBlue(blue + 1)}
-        onDecrease={() => setBlue(blue - 1)}
-        color="Blue"
+        onIncrease={() => setColor("green", COLOR_INCREMENT)}
+        onDecrease={() => setColor("green", -COLOR_INCREMENT)}
+        color="Green"
       />
       <ColorCounter
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setGreen(green - 1)}
-        color="Green"
+        onIncrease={() => setColor("blue", COLOR_INCREMENT)}
+        onDecrease={() => setColor("blue", -COLOR_INCREMENT)}
+        color="Blue"
       />
       <View
         style={{
@@ -36,7 +54,5 @@ const SquareScreen = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default SquareScreen;

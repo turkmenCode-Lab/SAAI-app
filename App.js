da36cfa4e87737e563854923230999c4f210b259
Merdan <wisew.tube@gmail.com>
2025-09-28 02:00:01 +0500

    init commit

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
@@ -11,14 +10,12 @@ import CounterScreen from "./src/screens/CounterScreen";
 import ColorScreen from "./src/screens/ColorScreen";
 import SquareScreen from "./src/screens/SquareScreen";
 
-const Tab = createBottomTabNavigator();
-
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator
+      <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{ headerTitle: "App" }}
       >
@@ -29,16 +26,7 @@ export default function App() {
         <Stack.Screen name="Counter" component={CounterScreen} />
         <Stack.Screen name="Color" component={ColorScreen} />
         <Stack.Screen name="Square" component={SquareScreen} />
-      </Stack.Navigator> */}
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Components" component={ComponentsScreen} />
-        <Tab.Screen name="List" component={ListScreen} />
-        <Tab.Screen name="Image" component={ImageScreen} />
-        <Tab.Screen name="Counter" component={CounterScreen} />
-        <Tab.Screen name="Color" component={ColorScreen} />
-        <Tab.Screen name="Square" component={SquareScreen} />
-      </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }

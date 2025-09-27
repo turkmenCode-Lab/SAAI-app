e8162421869bcbd6e128226b66ad05ce50bbcfc2
Merdan <wisew.tube@gmail.com>
2025-09-28 02:20:29 +0500

    init commit

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,28 +5,6 @@ import ColorCounter from "../components/ColorCounter";
 const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
-
-  const setColor = (color, change) => {
-    switch (color) {
-      case "red":
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        return;
-      case "green":
-        green + change > 255 || green + change < 0
-          ? null
-          : setGreen(green + change);
-        return;
-      case "blue":
-        blue + change > 255 || blue + change < 0
-          ? null
-          : setBlue(blue + change);
-        return;
-    }
-  };
-
   return (
     <View>
       <ColorCounter

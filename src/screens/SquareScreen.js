b1383b757aafec86fc2a814dfce6604b5bb4d95e
Merdan <wisew.tube@gmail.com>
2025-09-28 02:04:36 +0500

    init commit

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -4,8 +4,8 @@ import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+  const [green, setGreen] = useState(0);
 
   console.log(red);
 
@@ -16,8 +16,16 @@ const SquareScreen = () => {
         onDecrease={() => setRed(red - 1)}
         color="Red"
       />
-      <ColorCounter color="Blue" />
-      <ColorCounter color="Green" />
+      <ColorCounter
+        onIncrease={() => setBlue(blue + 1)}
+        onDecrease={() => setBlue(blue - 1)}
+        color="Blue"
+      />
+      <ColorCounter
+        onIncrease={() => setGreen(green + 1)}
+        onDecrease={() => setGreen(green - 1)}
+        color="Green"
+      />
     </View>
   );
 };

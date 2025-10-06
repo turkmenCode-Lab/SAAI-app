eaca6cfe4c23e2babbb5958e50c48f6e157bbede
Merdan <wisew.tube@gmail.com>
2025-10-06 19:33:33 +0500

    init commit

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -4,8 +4,13 @@ import {TextInput, View, StyleSheet} from "react-native";
 const Prompt = () => {
     return (
         <View style={styles.container}>
-        <TextInput/>
-
+            <TextInput
+                autoCorrect={false}
+                style={styles.input}
+                placeholder="Ask anything you want..."
+                placeholderTextColor="grey"
+                autoCapitalize="none"
+            />
         </View>
     )
 }
@@ -13,7 +18,18 @@ const Prompt = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    }
+    },
+    input: {
+        marginHorizontal: 15,
+        backgroundColor: '#222',
+        color: '#fff',
+        borderRadius: 22.5,
+        padding: 12,
+        marginBottom: 15,
+        fontSize: 16,
+        borderColor: '#FF6B00',
+        borderWidth: 1,
+    },
 })
 
 export default Prompt;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Prompt from "../components/Prompt";
 
 const HomeScreen = ({ navigation }) => {
   return (
       <SafeAreaView style={{ flex: 1 }}>
-
+            <Prompt/>
       </SafeAreaView>
   );
 };

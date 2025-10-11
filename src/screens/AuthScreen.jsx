c7c148c0d893caf5b8768dba7a5ea50005dcf5d8
Merdan <wisew.tube@gmail.com>
2025-10-11 23:40:17 +0500

    init commit

---
+++ src/screens/AuthScreen.jsx
@@ -0,0 +1,9 @@
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+
+const AuthScreen = () => {
+  return null;
+};
+
+const styles = StyleSheet.create({});
+
+export default AuthScreen;
